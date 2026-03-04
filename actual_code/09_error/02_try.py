38c817c8fd811817613dba01e6fd77240b639939
Kevin Cunningham <kevin@kevincunningham.co.uk>
2026-03-04 13:15:26 +0000

    updates try

--- actual_code/09_error/02_try.py
+++ actual_code/09_error/02_try.py
@@ -6,11 +6,15 @@ def ten_divided_by_user_number(no_of_tries = 1):
     print("This failed!")
     return False
   try:
-    user_number = int(input("Give me a number (please only use digits): "))  # raised an error
+    user_number = input("Give me a number (please only use digits): ")  # raised an erro
+    while not user_number.isnumeric():
+      print("That wasn't a number")
+      user_number = input("Give me a number (please only use digits): ")  
+    user_number = int(user_number)
     print(user_number)
+
     print(f"10 divided by your number is {10 / user_number}")
     return 10 / user_number
-
   except ValueError as err:
     # tell the user this
     print("Sorry. That wasn't a number. Please only use digits.")
@@ -19,8 +23,8 @@ def ten_divided_by_user_number(no_of_tries = 1):
     return ten_divided_by_user_number(no_of_tries + 1)
   except ZeroDivisionError:
     print("Whoops! You can't divide by zero.")
+    print(traceback.print_exc(10))
     return ten_divided_by_user_number(no_of_tries + 1)
-
   except Exception as err:  # bare/naked except!
     print("Something went wrong.")
     print(f"{type(err)}: {err}")
